adde52808a5002323aea18095cddbe155cb7ec36
Nina Kin <matikin9@gmail.com>
2022-12-18 13:52:52 -0800

    update for taiko page and doily gallery

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+const isProduction = process.env.NODE_ENV === "prod";
+
+module.exports = function(eleventyConfig) {
+
+	eleventyConfig.addPassthroughCopy("src/img");
+	eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPassthroughCopy("src/js");
+
+	// eleventyConfig.setBrowserSyncConfig({
+	// 	files: './docs/css/**/*.css'
+	// });
+
+	return {
+		pathPrefix: "/nina-website/",
+		dir: {
+            input: "src",
+			output: "docs"
+		}
+	}
+}
