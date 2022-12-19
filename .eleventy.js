95e7fe1bd736244ac2bcf49016a034bf9dcea3fd
Nina Kin <matikin9@gmail.com>
2022-12-18 23:58:08 -0800

    add prod flag for pathPrefix

--- .eleventy.js
+++ .eleventy.js
@@ -11,7 +11,7 @@ module.exports = function(eleventyConfig) {
 	// });
 
 	return {
-		pathPrefix: "/nina-website/",
+		pathPrefix: isProduction ? "" : "/nina-website/",
 		dir: {
             input: "src",
 			output: "docs"
